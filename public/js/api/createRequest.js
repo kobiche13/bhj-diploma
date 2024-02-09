c8b191d72f354250aaf1f79c73be0567317d7df9
Artem Serebriakov <koba156@yandex.ru>
2024-02-10 01:11:10 +0300

    Редактирую API

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -5,40 +5,46 @@
 const createRequest = (options = {}) => {
   let url = options.url;
   let formData = new FormData();
-  let method;
+  let method = options.method;
   for (let key in options.data) {
     formData.append(key, options.data[key]);
   }
   const xhr = new XMLHttpRequest();
   try {
+    
     xhr.open(method, url);
-    xhr.responseType = 'json';
-    if (options.callback) {
-      xhr.addEventListener('readystatechange', function () {
-        if (this.readyState == xhr.DONE) {
+    xhr.responseType = 'json';   
+    xhr.send(formData); 
+    /*xhr.addEventListener('readystatechange', function() {
+      if (this.readyState == xhr.DONE) {
           let response = this.response;
           if (this.status == 200 || this.status == 201) {
-            options.callback(response);
+            options.callback(response);                
           }
-        }
-      })
-    }
-    xhr.send(formData);
+        }        
+    })
+    */
   }
-  catch (err) {
-    options.callback(err);
+  catch(err) {    
+    console.log(this.err);
   }
 }
 
 createRequest({
-  url: '/user', // адрес
+  url : '/user', // адрес
   data: { // произвольные данные, могут отсутствовать
     email: 'ivan@poselok.ru',
     password: 'odinodin'
   },
   method: 'POST',
   callback: (err, response) => {
-    if (err === null){    
+    /*if (err === null){    
+      console.log( 'Данные, если нет ошибки', response );
+    } else {
+      console.log( 'Ошибка, если есть', err );
+    }*/
+    if (response !== null){    
+      err === null;
       console.log( 'Данные, если нет ошибки', response );
     } else {
       console.log( 'Ошибка, если есть', err );
